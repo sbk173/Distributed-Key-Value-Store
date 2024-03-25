7e79f4365f5501882c454f03688c558f18c63109
unknown <sathvik.vittal@gmail.com>
2024-03-25 18:47:49 +0530

    Documentation of the code. Each functionality is described

--- app.py
+++ app.py
@@ -1,25 +1,49 @@
 from flask import Flask, render_template, request
-from client import Client
+from client import Client    # Import the Client class from client.py file
 
 app = Flask(__name__)
 client = Client()  # Instantiate the client
 
+
 # Route to display available options
 @app.route('/')
 def index():
+    """
+    Renders the index.html template.
+
+    Returns:
+        str: HTML content for the index page.
+    """
+        
     return render_template('index.html')
+
 
 # Route for put operation
 @app.route('/put', methods=['POST'])
 def put():
+    """
+    Handles the PUT operation to store a key-value pair in the etcd server.
+
+    Returns:
+        str: Confirmation message indicating the success of the operation.
+    """
+
     key = request.form['key']
     value = request.form['value']
     client.put_key(key, value)
     return "Key '{}' with value '{}' added successfully.".format(key, value)
 
+
 # Route for get operation
 @app.route('/get', methods=['POST'])
 def get():
+    """
+    Handles the GET operation to retrieve the value of a key from the etcd server.
+
+    Returns:
+        str: The value associated with the requested key.
+    """
+
     key = request.form['key']
     try:
         value = client.get_value(key)
@@ -27,9 +51,17 @@ def get():
     except Exception as e:
         return e
 
+
 # Route for delete operation
 @app.route('/delete', methods=['POST'])
 def delete():
+    """
+    Handles the DELETE operation to remove a key-value pair from the etcd server.
+
+    Returns:
+        str: Confirmation message indicating the success of the operation.
+    """
+
     key = request.form['key']
     try:
         message = client.delete_key(key)
@@ -40,6 +72,13 @@ def delete():
 # Route for list operation
 @app.route('/list')
 def list_keys():
+    """
+    Lists all keys stored in the etcd server.
+
+    Returns:
+        str: HTML content displaying the list of keys.
+    """
+     
     keys = client.get_all_keys()
     return render_template('list.html', keys=keys)
 
--- client.py
+++ client.py
@@ -1,11 +1,33 @@
 import etcd3
 
 class Client:
+    """
+    A class for interacting with an etcd server.
+    
+    Attributes:
+        client (etcd3.Client): An instance of etcd3 client.
+    """
     client = None
+
     def __init__(self, host='localhost', port=2379):
+        """
+        Initializes the Client object.
+        
+        Args:
+            host (str): The host address of the etcd server. Default is 'localhost'.
+            port (int): The port number of the etcd server. Default is 2379.
+        """
+
         self.client = etcd3.client(host=host, port=port)
 
     def get_all_keys(self):
+        """
+        Retrieves all keys from the etcd server.
+        
+        Returns:
+            list: A list of tuples containing key-value pairs.
+        """
+
         keys = self.client.get_all()
         keys = list(keys)
         key_set = []
@@ -18,6 +40,16 @@ class Client:
         
 
     def get_value(self, key):
+        """
+        Retrieves the value corresponding to a given key from the etcd server.
+        
+        Args:
+            key (str): The key whose value is to be retrieved.
+            
+        Returns:
+            str: The value associated with the given key.
+        """
+        
         try:
             value, metadata = self.client.get(key)
             return key + " : " + value.decode('utf-8')
@@ -27,9 +59,30 @@ class Client:
             return "Error getting value for key '{}': {}".format(key, e)
 
     def put_key(self, key, value):
+        """
+        Stores a key-value pair in the etcd server.
+        
+        Args:
+            key (str): The key to be stored.
+            value (str): The value associated with the key.
+
+        Returns:
+            None
+        """
+
         self.client.put(key, value)
 
     def delete_key(self, key):
+        """
+        Deletes a key-value pair from the etcd server.
+        
+        Args:
+            key (str): The key to be deleted.
+            
+        Returns:
+            str: Confirmation message indicating whether the deletion was successful.
+        """
+        
         key_set = self.get_all_keys()
         keys = []
         for i in key_set:
